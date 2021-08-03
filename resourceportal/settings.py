5f4dbaecc4cb243d3039e67e680b82428bff05a6
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-08-03 21:14:49 +0530

    Deleted staticfiles

--- resourceportal/settings.py
+++ resourceportal/settings.py
@@ -162,9 +162,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'resourceportal.storage_backends.MediaStorage'
